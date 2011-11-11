41dd9a4a544e29f00354e1489cba32fcbf08db29
fede.47@gmail.com <fede.47@gmail.com@dca5db65-3d8f-aefd-1fdf-d36b09358e26>
2011-11-11 04:54:45 +0000

    ahora sí, copio lo de jorge que antes me salió mal

---
+++ trunk/tp2/src/prueba/pruebita.java
@@ -0,0 +1,14 @@
+package prueba;
+
+public class pruebita {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("esto es una prueba");
+		System.out.print("un saludo mas menos");
+
+	}
+
+}
