bdc0b13e257f5ccbc8376ee16f474f52ea7ef212
fede.47@gmail.com <fede.47@gmail.com@dca5db65-3d8f-aefd-1fdf-d36b09358e26>
2011-11-12 19:09:22 +0000

    lean "LEER.txt"... tomás, si me mandé alguna cagada avisá :P

---
+++ trunk/src/modelo/Posicion.java
@@ -0,0 +1,24 @@
+package modelo;
+
+/**
+ * Modela a un vector que representa una posición en un plano.
+ * @author Tomás
+ *
+ */
+public class Posicion extends Vector {
+
+	public Posicion(int x, int y) {
+		super(x,y);
+	}
+	
+	/**
+	 * Realiza la resta coordenada a coordenada entre el vector representado por este objeto y el representado por el parámetro.
+	 * @param posicion instancia de la clase Posicion que restaremos a esta 
+	 * @return instancia de la clase Posicion que representa la resta de esta instancia y la del parámetro
+	 */
+	public Posicion restar(Posicion posicion) {
+		Posicion posicionAuxiliar = new Posicion(x - posicion.getX(),y - posicion.getY());
+		return posicionAuxiliar;
+	}
+
+}
---
+++ trunk/src/pruebas/PruebasOrientacion.java
@@ -0,0 +1,50 @@
+package pruebas;
+
+import modelo.Orientacion;
+import junit.framework.TestCase;
+
+/**
+ * Llevo a cabo las pruebas unitarias de la clase Orientacion.
+ * @author Tomás
+ *
+ */
+public class PruebasOrientacion extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * Testeo si los atributos de la orientacion i tienen los valores correctos.
+	 */
+	public void testOrientacionICreacionCorrecta() {
+		assertEquals(Orientacion.i.getX(),1);
+		assertEquals(Orientacion.i.getY(),0);
+	}
+	
+	/**
+	 * Testeo si los atributos de la orientacion iNegativo tienen los valores correctos.
+	 */
+	public void testOrientacionINegativoCreacionCorrecta() {
+		assertEquals(Orientacion.iNegativo.getX(),-1);
+		assertEquals(Orientacion.iNegativo.getY(),0);
+	}
+	
+	/**
+	 * Testeo si los atributos de la orientacion j tienen los valores correctos.
+	 */
+	public void testOrientacionJCreacionCorrecta() {
+		assertEquals(Orientacion.j.getX(),0);
+		assertEquals(Orientacion.j.getY(),1);
+	}
+	
+	/**
+	 * Testeo si los atributos de la orientacion jNegativo tienen los valores correctos.
+	 */
+	public void testOrientacionJNegativoCreacionCorrecta() {
+		assertEquals(Orientacion.jNegativo.getX(),0);
+		assertEquals(Orientacion.jNegativo.getY(),-1);
+	}	
+	
+	
+}
