3c788d2d0293762e3756d72f98c96c1f0b5fc97a
jorgecollinet@hotmail.com <jorgecollinet@hotmail.com@dca5db65-3d8f-aefd-1fdf-d36b09358e26>
2011-11-17 03:04:00 +0000

    cree una clase abstracta bonus y uno de los bonuses(bonusVida)

--- trunk/src/modelo/tanques/Tanque.java
+++ trunk/src/modelo/tanques/Tanque.java
@@ -13,8 +13,8 @@ import modelo.manejoEspacial.Orientacion;
 import modelo.manejoEspacial.Posicion;
 
 /**
- * Provee el estado y el comportamiento común a todos los tanques del juego.
- * @author Tomás
+ * Provee el estado y el comportamiento comï¿½n a todos los tanques del juego.
+ * @author Tomï¿½s
  *
  */
 public abstract class Tanque extends ObjetoMovil {
@@ -24,7 +24,7 @@ public abstract class Tanque extends ObjetoMovil {
 	
 	/**
 	 * Constructor.
-	 * @param puntoMenorModulo punto de menor módulo de la ocupación del tanque
+	 * @param puntoMenorModulo punto de menor mï¿½dulo de la ocupaciï¿½n del tanque
 	 */
 	public Tanque(Posicion puntoMenorModulo) {
 		ocupacion = new OcupacionCuadrada(puntoMenorModulo,5);
@@ -32,7 +32,7 @@ public abstract class Tanque extends ObjetoMovil {
 	}
 	
 	/**
-	 * Cuando un tanque choca con un disparo es dañado por este. Si su resistencia es menor o igual que 0 entonces desaparece.
+	 * Cuando un tanque choca con un disparo es daï¿½ado por este. Si su resistencia es menor o igual que 0 entonces desaparece.
 	 */
 	public void chocarConDisparo(Disparo disparo) {
 		resistencia = resistencia - disparo.getDanioNeto();
@@ -42,7 +42,7 @@ public abstract class Tanque extends ObjetoMovil {
 	}
 	
 	/**
-	 * Cuando un tanque choca a este, como este obstaculiza su camino, le indicamos al otro que haga un movimiento unitario en la orientación contraria a la que tiene.
+	 * Cuando un tanque choca a este, como este obstaculiza su camino, le indicamos al otro que haga un movimiento unitario en la orientaciï¿½n contraria a la que tiene.
 	 */
 	public void chocarConTanque(Tanque tanque) {
 		tanque.moverEnDireccionContraria();
@@ -65,8 +65,13 @@ public abstract class Tanque extends ObjetoMovil {
 		return resistencia;
 	}
 	
+	
+	public void sumarResistencia(int resistenciaASumar){
+		resistencia += resistenciaASumar;
+	}
+	
 	/**
-	 * En este caso, si la ocupación no es válida, significa que un tanque ha tratado de moverse hacia fuera de un límite de la pantalla, y por lo tanto no se le setea su nueva ocupación para no permitírselo.
+	 * En este caso, si la ocupaciï¿½n no es vï¿½lida, significa que un tanque ha tratado de moverse hacia fuera de un lï¿½mite de la pantalla, y por lo tanto no se le setea su nueva ocupaciï¿½n para no permitï¿½rselo.
 	 */
 	protected void chequearOcupacionValidaYColisiones(Ocupacion ocupacionProvisoria) {
 		if ((ocupacionProvisoria.espacialmenteValida())) {
@@ -81,7 +86,7 @@ public abstract class Tanque extends ObjetoMovil {
 	}
 	
 	/**
-	 * Indica al objeto que haga un movimiento unitario en una dirección contraria a la que trae.
+	 * Indica al objeto que haga un movimiento unitario en una direcciï¿½n contraria a la que trae.
 	 */
 	public void moverEnDireccionContraria() {
 		if (orientacion == Orientacion.i)
---
+++ trunk/src/pruebas/pruebasUnitarias/PruebaBonusVida.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package pruebas.pruebasUnitarias;
+
+import modelo.bonuses.BonusVida;
+import modelo.manejoEspacial.Posicion;
+import modelo.tanques.AlgoTank;
+import modelo.tanques.Tanque;
+import junit.framework.TestCase;
+
+
+
+/**
+ * @author jc
+ *
+ */
+public class PruebaBonusVida extends TestCase {
+	BonusVida bonusVida;
+	Tanque tanque;
+	double porcentajeQueAumentaBonus;
+	
+	public void setUp (){
+		Posicion puntoCualquiera = new Posicion(0,0);
+		this.tanque = new AlgoTank(puntoCualquiera);
+		this.bonusVida = new BonusVida (puntoCualquiera);
+		this.porcentajeQueAumentaBonus = 0.4;
+	}
+	
+	public void testBonusVida(){
+		int resistenciaAntes = this.tanque.getResistencia();
+		this.bonusVida.chocarConTanque(this.tanque);
+		int resistenciaDespues = this.tanque.getResistencia();
+		
+		assert( (resistenciaAntes += resistenciaAntes*porcentajeQueAumentaBonus) == resistenciaDespues);
+	}
+}
