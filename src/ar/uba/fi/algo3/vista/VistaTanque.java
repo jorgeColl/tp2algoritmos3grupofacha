9dedc8fff3b0499cabb61f346aaa72de79c1398f
fede.47 <fede.47@gmail.com>
2011-11-28 23:25:04 +0000

    señores paren todo: nuestro cuadrado azul feo se mueve, snif.

--- src/ar/uba/fi/algo3/vista/VistaTanque.java
+++ src/ar/uba/fi/algo3/vista/VistaTanque.java
@@ -4,7 +4,9 @@
 package ar.uba.fi.algo3.vista;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 
+import ar.uba.fi.algo3.modelo.tanques.Tanque;
 import ar.uba.fi.algo3.titiritero.vista.Cuadrado;
 
 /**
@@ -16,6 +18,27 @@ public class VistaTanque extends Cuadrado {
 	public VistaTanque(){
 		super(25,25);
 		setColor(Color.BLUE);
+	}
+	
+	public void keyPressed(KeyEvent keyEvent){
+		int c = keyEvent.getKeyCode();
+		Tanque posicionable = (Tanque) this.getPosicionable();
+		switch (c){
+			case KeyEvent.VK_RIGHT:
+				posicionable.moverDerecha();
+				break;
+			case KeyEvent.VK_LEFT:
+				posicionable.moverIzquierda();
+				break;
+			case KeyEvent.VK_UP:
+				posicionable.moverArriba();
+				break;
+			case KeyEvent.VK_DOWN:
+				posicionable.moverAbajo();
+				break;
+		}
+		
+		
 	}
 
 }
