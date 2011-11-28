ac275af785417143be3060d7f1070f1658901496
fede.47 <fede.47@gmail.com>
2011-11-28 22:57:14 +0000

    va moo recibimos caracteres

--- src/ar/uba/fi/algo3/titiritero/vista/Figura.java
+++ src/ar/uba/fi/algo3/titiritero/vista/Figura.java
@@ -1,13 +1,15 @@
 package ar.uba.fi.algo3.titiritero.vista;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 import ar.uba.fi.algo3.titiritero.Dibujable;
 import ar.uba.fi.algo3.titiritero.MouseClickObservador;
+import ar.uba.fi.algo3.titiritero.KeyPressedObservador;
 import ar.uba.fi.algo3.titiritero.Posicionable;
 import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
 
-public abstract class Figura implements Dibujable, MouseClickObservador {
+public abstract class Figura implements Dibujable, MouseClickObservador, KeyPressedObservador {
 
 	private Color color;
 	private Posicionable posicionable;
@@ -32,6 +34,13 @@ public abstract class Figura implements Dibujable, MouseClickObservador {
 
 	public void MouseClick(int x, int y){
 		System.out.println("Click;" + x + "," + y);
+	}
+	
+	public void keyPressed(KeyEvent keyEvent){
+		char c = keyEvent.getKeyChar();
+		if(c != keyEvent.CHAR_UNDEFINED){
+			System.out.println("tecla apretada:" + c);
+		}
 	}
 		
 }
--- src/ar/uba/fi/algo3/titiritero/vista/Ventana.java
+++ src/ar/uba/fi/algo3/titiritero/vista/Ventana.java
@@ -22,7 +22,6 @@ public class Ventana extends Frame implements SuperficieDeDibujo{
 	 * Esta yerba es generada automática y aún no se su utilidad
 	 */
 	private static final long serialVersionUID = 1L;
-
 	
 	private ControladorJuego controlador;
 	
@@ -63,6 +62,7 @@ public class Ventana extends Frame implements SuperficieDeDibujo{
 	
 	public Ventana(int ancho,int alto, ControladorJuego unControlador){
 		this.addMouseListener(new MouseClickController(unControlador));
+		this.addKeyListener(new KeyPressedController(unControlador));
 		setSize(ancho, alto);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		Dimension frameSize = getSize();
