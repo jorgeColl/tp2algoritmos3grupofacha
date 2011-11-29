cb901b3e0165e86ea8410a58afb3d144f2f019d6
samiloiza@gmail.com <samiloiza@gmail.com@dca5db65-3d8f-aefd-1fdf-d36b09358e26>
2011-11-29 03:03:06 +0000

    En orientación cambio la visibilidad del creador, para iniciar cualquier Disparo se requiere una orientación.

--- trunk/src/ar/uba/fi/algo3/modelo/manejoEspacial/Orientacion.java
+++ trunk/src/ar/uba/fi/algo3/modelo/manejoEspacial/Orientacion.java
@@ -13,7 +13,7 @@ public class Orientacion extends Vector {
 	public static Orientacion j = new Orientacion(0,1);
 	public static Orientacion jNegativo = new Orientacion(0,-1);
 	
-	private Orientacion(int x, int y) {
+	public Orientacion(int x, int y) {
 		super(x, y);
 	}
 
