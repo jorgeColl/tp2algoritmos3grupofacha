70a465d7b69bf7d6c1eea88b8ac7d13d14360f37
samiloiza <samiloiza@gmail.com>
2011-11-29 03:03:06 +0000

    En orientación cambio la visibilidad del creador, para iniciar cualquier Disparo se requiere una orientación.

--- src/ar/uba/fi/algo3/CorrerJuego.java
+++ src/ar/uba/fi/algo3/CorrerJuego.java
@@ -3,6 +3,9 @@
  */
 package ar.uba.fi.algo3;
 
+
+import ar.uba.fi.algo3.modelo.armamentista.disparo.DisparoAmetralladora;
+import ar.uba.fi.algo3.modelo.manejoEspacial.Orientacion;
 import ar.uba.fi.algo3.modelo.manejoEspacial.Posicion;
 import ar.uba.fi.algo3.modelo.objetosInanimados.CuartelArgentino;
 import ar.uba.fi.algo3.modelo.objetosInanimados.ParedConcreto;
@@ -10,6 +13,7 @@ import ar.uba.fi.algo3.modelo.tanques.AlgoTank;
 import ar.uba.fi.algo3.modelo.objetosInanimados.BonusVelocidad;
 import ar.uba.fi.algo3.titiritero.ControladorJuego;
 import ar.uba.fi.algo3.titiritero.vista.Ventana;
+import ar.uba.fi.algo3.vista.VistaDisparo;
 import ar.uba.fi.algo3.vista.VistaTanque;
 import ar.uba.fi.algo3.vista.VistaPared;
 import ar.uba.fi.algo3.vista.VistaBonus;
@@ -34,6 +38,7 @@ public class CorrerJuego {
 		
 		ControladorJuego controlador = new ControladorJuego(activarReproductor);
 		Ventana ventana = new Ventana(800, 600, controlador);
+		ventana.setTitle("AlgoTank");
 		
 		/* líneas copiadas del ejemplo. */
 		controlador.setSuperficieDeDibujo(ventana);
@@ -50,19 +55,26 @@ public class CorrerJuego {
 		BonusVelocidad bonusVel = new BonusVelocidad(new Posicion(30, 30));
 		VistaBonus vistaBonusVel = new VistaBonus();
 		
-		CuartelArgentino cuartel = new CuartelArgentino(new Posicion(100, 400));
+		CuartelArgentino cuartel = new CuartelArgentino(new Posicion(200, 300));
 		VistaCuartelArgentino vistaCuartel = new VistaCuartelArgentino();
+		
+		DisparoAmetralladora disparo = new DisparoAmetralladora(new Orientacion(1,0),new Posicion(350,150));
+		VistaDisparo vistaDisparo = new VistaDisparo();
+		
 		
 		vistaPared.setPosicionable(paredC);
 		vistaTanque.setPosicionable(algoTank);
 		vistaBonusVel.setPosicionable(bonusVel);
 		vistaCuartel.setPosicionable(cuartel);
+		vistaDisparo.setPosicionable(disparo);
 		
 		controlador.agregarObjetoVivo(algoTank);
+		controlador.agregarObjetoVivo(disparo);
 		controlador.agregarDibujable(vistaTanque);		
 		controlador.agregarDibujable(vistaPared);
 		controlador.agregarDibujable(vistaBonusVel);
 		controlador.agregarDibujable(vistaCuartel);
+		controlador.agregarDibujable(vistaDisparo);
 		
 		controlador.agregarKeyPressObservador(vistaTanque);
 		
---
+++ src/ar/uba/fi/algo3/vista/VistaDisparo.java
@@ -0,0 +1,14 @@
+package ar.uba.fi.algo3.vista;
+
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class VistaDisparo extends Imagen{
+	
+	public VistaDisparo(){
+		super();
+		this.setNombreArchivoImagen("img/Disparo.jpg");
+		
+		
+	}
+
+}
