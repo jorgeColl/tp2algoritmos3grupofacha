bf2b6926c77c81467d27b12f3828b570bf81d097
jorgecollinet <jorgecollinet@hotmail.com>
2011-12-01 02:10:29 +0000

    aca trate de que la vista de el disparo cuando este se destruya , la vista antes de dibujar se va a fijar si esta vivo o no y si esta muerto la vista se va a autoeliminar, me gustaria hacer despues esta accion mas generalizada para todas las vistas del juego

--- src/ar/uba/fi/algo3/vista/VistaDisparo.java
+++ src/ar/uba/fi/algo3/vista/VistaDisparo.java
@@ -1,5 +1,8 @@
 package ar.uba.fi.algo3.vista;
 
+import ar.uba.fi.algo3.modelo.clasesGeneralizadoras.ObjetoJuego;
+import ar.uba.fi.algo3.titiritero.ControladorJuego;
+import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
 import ar.uba.fi.algo3.titiritero.vista.ImagenEstatica;
 
 public class VistaDisparo extends ImagenEstatica{
@@ -7,6 +10,14 @@ public class VistaDisparo extends ImagenEstatica{
 	public VistaDisparo(){
 		super();
 		this.setNombreArchivoImagen("img/Disparo.JPG");
+	}
+	public void dibujar(SuperficieDeDibujo superficeDeDibujo){
+		if( ((ObjetoJuego)this.posicionable).isVivo() == false){
+			//ACA EXPLOTA TODO :( 
+			//ControladorJuego.getInstancia().removerDibujable(this);
+		}else{
+			super.dibujar(superficeDeDibujo);
+		}
 	}
 
 }
